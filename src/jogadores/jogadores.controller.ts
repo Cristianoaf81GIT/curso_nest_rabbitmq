ec2098503b5b268f89fcf4ff6e16baed0d00254c
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-09-13 07:26:27 -0300

    aula(12): criando controller jogadores PII

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDTO } from '../dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogador() {
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDTO) {
+    const { email } = criarJogadorDto;
     return JSON.stringify({
-      nome: 'Cristiano',
+      email: `${email}`,
     });
   }
 }
