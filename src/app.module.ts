7bf67cc815bad147cd5de0afd674e81ca381ab0d
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-09-27 06:58:48 -0300

    aula(20): persistencia dos dados parte I

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://cristianoaf81:mongoAtlasCr2020@cluster0.cmkuy.gcp.mongodb.net/smartRanking?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
