3c9649a0276de7749a9715f8f0a7939b1047d3b4
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-10-12 13:02:14 -0300

    aula(27): Entidade Categorias parte I

---
+++ src/categorias/categorias.controller.ts
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  Body,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Categoria } from '../interfaces/categoria.interface';
+import { CriarCategoriaDto } from '../dtos/criar-categoria.dto';
+import { CategoriasService } from './categorias.service';
+
+@Controller('api/v1/categorias')
+export class CategoriasController {
+  constructor(private readonly categoriaService: CategoriasService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async criarCategoria(
+    @Body() criarCategoriaDto: CriarCategoriaDto,
+  ): Promise<Categoria> {
+    return await this.categoriaService.criarCategoria(criarCategoriaDto);
+  }
+}
---
+++ src/categorias/categorias.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriaSchema } from '../interfaces/categoria.schema';
+import { CategoriasController } from './categorias.controller';
+import { CategoriasService } from './categorias.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }]),
+  ],
+  controllers: [CategoriasController],
+  providers: [CategoriasService],
+})
+export class CategoriasModule {}
